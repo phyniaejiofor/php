3e8d5e5de30b601124f78a7d71e852246cfbed61
phyniaejiofor <phyniaejiofor90@gmail.com>
2021-01-29 10:42:13 +0100

    Code push on strings

---
+++ strings.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings</title>
+</head>
+<body>
+    <?php
+
+    echo "Hello World!<br />";
+    echo 'Hello World!<br />';
+    
+    $greeting = "Hello";
+    $target = "World!";
+    $phrase = $greeting . " " . $target;
+    echo $phrase;
+    ?>
+
+    <br/>
+
+    <?php
+
+    echo "$phrase Again<br/>";
+
+    echo '$phrase again<br />';
+
+    echo "${phrase}Again<br/>";
+    
+    
+    
+    ?>
+</body>
+</html>

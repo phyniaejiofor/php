b49fd2ac663aa96152cfde3241a93410170fa986
phyniaejiofor <phyniaejiofor90@gmail.com>
2021-01-29 09:09:42 +0100

    This is about request respond cycle in php

---
+++ operationalTrial.php
@@ -0,0 +1,6 @@
+<!-- Operational trial -->
+This is a request respond cycle in php. 
+ It decribes how the browser and the web server communicates to process user request.  
+ The browser makes a request to the web server(localhost), the web server software (Apache) needs to be there to grant the request. 
+ Apache sees the request(php file), process the request, go to Database to get some information and also store some information,
+ after processing the php file, a html file which will be returned as an output and send back to the browser.

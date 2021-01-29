f1a063d24b9f9026e3afb99c23e2aededae65a1f
phyniaejiofor <phyniaejiofor90@gmail.com>
2021-01-29 09:41:51 +0100

    How to comment on php files

---
+++ codeComments.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    // Single-line comments are like this 
+    # or like this (less common)
+
+    /* double line comments are written like this so that you can keep on typing and typing */
+    
+    
+    ?>
+
+    <?php echo "Hello World!" ;?> <br/>
+    <?php echo "Hello" . "World!";?> <br />
+    <?php echo "I love you!";?>
+
+</body>
+</html>

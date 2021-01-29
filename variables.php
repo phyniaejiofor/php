c5c4531d27ac79ee11afce0985a0e4a317d18bec
phyniaejiofor <phyniaejiofor90@gmail.com>
2021-01-29 10:12:56 +0100

    code on how to declare varaibles in php

---
+++ variables.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php
+
+    $var1 = 20;
+    echo $var1;
+
+    echo "<br />";
+
+    $var1 = 500;
+    echo $var1;
+
+    echo "<br />";
+
+    $var2 = "Hi Phynia";
+    echo $var2;
+    
+    
+    
+    ?>
+</body>
+</html>

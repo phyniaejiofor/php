927c102a9fe6002a076231c2a6bb59aebbd49b2a
phyniaejiofor <phyniaejiofor90@gmail.com>
2021-01-31 21:07:07 +0100

    Code push on operators

---
+++ operators.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comparison and Logical Operators</title>
+</head>
+<body>
+    
+<?php
+$a = 4;
+$b = 3;
+$c = 1;
+$d = 20;
+
+if (($a >= $b) || ($c >= $d)) {
+   echo "a is larger than b OR";
+   echo " c is larger than d";
+}
+?>
+<br/>
+
+<?php
+   // $e = 100;   NB when $e is not set initially
+  if (!isset($e)) {
+     $e = 200;
+  }
+    echo $e;
+?>
+<br/>
+
+<?php
+  // dont reject 0 or 0.0
+  $quantity = "";
+  if (empty($quantity) && !is_numeric($quantity)) {
+      echo "You must enter a quantity";
+  }
+
+?>
+</body>
+</html>

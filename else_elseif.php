02eeac197246d30e96ddbf360615b4b6f4c3a74b
phyniaejiofor <phyniaejiofor90@gmail.com>
2021-01-31 20:12:18 +0100

    Code push on else and else if statement

---
+++ else_elseif.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Else and Else If</title>
+</head>
+<body>
+    
+<?php
+$a = 4;
+$b = 3;
+
+if ( $a > $b) {
+ echo "a is larger than b";
+}elseif ( $a < $b) {
+    echo "a is smaller than b";
+   }else{
+       echo "a is equal to b";
+   }
+
+?>
+</body>
+</html>

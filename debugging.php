1ba4261eb44ef67116492482b9299586aa9e417a
phyniaejiofor <phyniaejiofor90@gmail.com>
2021-02-15 13:34:05 +0100

    debugging and troubleshooting

---
+++ debugging.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Debugging</title>
+</head>
+<body>
+    
+<?php
+  $number = 99;
+  $string = "Bug?";
+  $array = array(1 => "Homepage", 2 => "About Us", 3 => "Services");
+   
+
+   var_dump($number);
+   var_dump($string);
+   var_dump($array);
+   
+?>
+
+<br/>
+   <pre>
+  <?php
+     // print_r(get_defined_vars()) ;
+  
+  ?>
+  </pre>
+  <br/>
+
+  <pre>
+  <?php
+       function say_hello_to($word){
+           echo "Hello {$word}! <br/>";
+           var_dump(debug_backtrace());
+       }
+        say_hello_to('Everyone')
+  ?>
+  </pre>
+ </body>
+</html>
